4d928814dcfa75ee4210a04c1a00441db05957b9
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-11-27 09:37:41 -0300

    enum TipoEndereco criado

--- src/main/java/duarte/lojavirtual/duarte/lojavirtual/model/Endereco.java
+++ src/main/java/duarte/lojavirtual/duarte/lojavirtual/model/Endereco.java
@@ -1,5 +1,7 @@
 package duarte.lojavirtual.duarte.lojavirtual.model;
 
+import duarte.lojavirtual.duarte.lojavirtual.enums.TipoEndereco;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
@@ -27,8 +29,19 @@ public class Endereco implements Serializable {
     @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
     private Pessoa pessoa;
 
+
+    @Enumerated(EnumType.STRING)
+    private TipoEndereco tipoEndereco;
     public Long getId() {
         return id;
+    }
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
     }
 
     public void setId(Long id) {
