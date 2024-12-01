e2b8bbbbfb005a08fa6df18e6fcbb332bd8545af
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-12-01 17:51:56 -0300

    Model ImagemProduto criada

---
+++ src/main/java/duarte/lojavirtual/duarte/lojavirtual/model/imagemProduto.java
@@ -0,0 +1,73 @@
+package duarte.lojavirtual.duarte.lojavirtual.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "imagem_produto")
+@SequenceGenerator(name = "seq_imagem_produto", sequenceName = "seq_imagem_produto", allocationSize = 1, initialValue = 1)
+public class imagemProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_imagem_produto")
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String imagemOrinal;
+
+    @Column(columnDefinition = "text")
+    private String imagemMiniatura;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "produto_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+    private Produto produto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImagemOrinal() {
+        return imagemOrinal;
+    }
+
+    public void setImagemOrinal(String imagemOrinal) {
+        this.imagemOrinal = imagemOrinal;
+    }
+
+    public String getImagemMiniatura() {
+        return imagemMiniatura;
+    }
+
+    public void setImagemMiniatura(String imagemMiniatura) {
+        this.imagemMiniatura = imagemMiniatura;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        imagemProduto that = (imagemProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
