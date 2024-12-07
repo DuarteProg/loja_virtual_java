6ecb91fad69d3f6b5e5d95ea8a849fafaf894046
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-12-07 19:02:50 -0300

    configurando campos obrigatórios na model Pessoa e PessoaJuridica

--- src/main/java/duarte/lojavirtual/duarte/lojavirtual/model/PessoaJuridica.java
+++ src/main/java/duarte/lojavirtual/duarte/lojavirtual/model/PessoaJuridica.java
@@ -2,6 +2,7 @@ package duarte.lojavirtual.duarte.lojavirtual.model;
 
 import duarte.lojavirtual.duarte.lojavirtual.model.Pessoa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
@@ -13,11 +14,20 @@ public class PessoaJuridica extends Pessoa {
 
     private static final long serialVersionUID = 1L;
 
+    @Column(nullable = false)
     private String cnpj;
+
+    @Column(nullable = false)
     private String inscEstadual;
+
     private String inscMunicipal;
+
+    @Column(nullable = false)
     private String nomeFantasia;
+
+    @Column(nullable = false)
     private String razaoSocial;
+
     private String categoria;
 
     public String getCnpj() {
