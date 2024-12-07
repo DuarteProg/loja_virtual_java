be0d21acce3f3e377f2d6081283cd019745181a3
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-12-07 19:10:07 -0300

    configurando campos obrigatórios na model Pessoa e Usuario

--- src/main/java/duarte/lojavirtual/duarte/lojavirtual/model/Usuario.java
+++ src/main/java/duarte/lojavirtual/duarte/lojavirtual/model/Usuario.java
@@ -20,10 +20,13 @@ public class Usuario implements UserDetails {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
     private Long id;
 
+    @Column(nullable = false)
     private String login;
 
+    @Column(nullable = false)
     private String senha;
 
+    @Column(nullable = false)
     @Temporal(TemporalType.DATE)
     private Date dataAtualSenha;
 
