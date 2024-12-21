d5936d3cf18cfcc5fa28afd615e8b5ba1c80520f
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-12-21 19:09:23 -0300

    ApplicationContextLoad criado

--- src/main/java/duarte/lojavirtual/duarte/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/duarte/lojavirtual/duarte/lojavirtual/security/WebConfigSecurity.java
@@ -1,11 +1,15 @@
 package duarte.lojavirtual.duarte.lojavirtual.security;
 
+import duarte.lojavirtual.duarte.lojavirtual.service.ImplementacaoUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.servlet.http.HttpSessionListener;
 
@@ -13,6 +17,15 @@ import javax.servlet.http.HttpSessionListener;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
+
+    @Autowired
+    private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(implementacaoUserDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
