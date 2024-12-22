5923a7c606174549397be8b07369f1d06ccf6b1f
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-12-22 11:50:57 -0300

    JWTTokenAutenticacaoService criado

---
+++ src/main/java/duarte/lojavirtual/duarte/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -0,0 +1,98 @@
+package duarte.lojavirtual.duarte.lojavirtual.security;
+
+import duarte.lojavirtual.duarte.lojavirtual.ApplicationContextLoad;
+import duarte.lojavirtual.duarte.lojavirtual.model.Usuario;
+import duarte.lojavirtual.duarte.lojavirtual.repository.UsuarioRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Service
+@Component
+public class JWTTokenAutenticacaoService {
+
+
+    private static final long EXPIRATION_TIME = 959990000;
+
+    //     Chave de senha para juntar com o JWT
+    private static final String SECRET = "secretpassword";
+
+    private static final String TOKEN_PREFIX = "Bearer";
+
+    private static final String HEADER_STRING = "Authorization";
+
+    //     Gera o token e da a resposta para o cliente
+    public void addAuthentication(HttpServletResponse response, String username) throws Exception {
+
+        String JWT = Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET).compact();
+
+        String token = TOKEN_PREFIX + " " + JWT;
+
+        response.addHeader(HEADER_STRING, token);
+
+        liberacaoCors(response);
+
+        response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
+    }
+
+    public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+        String token = request.getHeader(HEADER_STRING);
+
+        if (token != null) {
+
+            String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+
+            String user = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJwt(tokenLimpo)
+                    .getBody().getSubject();
+
+            if (user != null) {
+                Usuario usuario = ApplicationContextLoad
+                        .getApplicationContext()
+                        .getBean(UsuarioRepository.class).findUserByLogin(user);
+
+                if (usuario != null) {
+                    return new UsernamePasswordAuthenticationToken(
+                            usuario.getLogin(),
+                            usuario.getSenha(),
+                            usuario.getAuthorities());
+                }
+            }
+
+        }
+        liberacaoCors(response);
+        return null;
+    }
+
+
+    private void liberacaoCors(HttpServletResponse response) {
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Request-Headers") == null) {
+            response.addHeader("Access-Control-Request-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "*");
+        }
+    }
+
+}
