b61ebd54797cadda101fd8cd6501533537b1cf4a
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-12-26 11:49:13 -0300

    exception de erros do banco dde dados foram tratadas

--- src/main/java/duarte/lojavirtual/duarte/lojavirtual/security/JwtApiAutenticacaoFilter.java
+++ src/main/java/duarte/lojavirtual/duarte/lojavirtual/security/JwtApiAutenticacaoFilter.java
@@ -17,11 +17,18 @@ public class JwtApiAutenticacaoFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-        Authentication authentication = new JWTTokenAutenticacaoService()
-                .getAuthentication((HttpServletRequest) request, (HttpServletResponse) response);
+        try {
+            Authentication authentication = new JWTTokenAutenticacaoService()
+                    .getAuthentication((HttpServletRequest) request, (HttpServletResponse) response);
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        chain.doFilter(request, response);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().write("Ocorreu um erro no sistema, avise o administrador: \n" + e.getMessage());
+        }
+
+
     }
 }
